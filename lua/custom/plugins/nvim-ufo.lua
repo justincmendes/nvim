948969de90f5e3c015e75b2fe87d0dfcda5d8ec8
Justin Mendes <justincmendes@gmail.com>
2024-10-27 00:16:38 -0400

    Treesitter folds > LSP folds (+ added back foldtext)

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -8,6 +8,7 @@ return {
     vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
+    vim.o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend))]]
 
     -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
@@ -21,7 +22,7 @@ return {
 
     require('ufo').setup {
       provider_selector = function(bufnr, filetype, buftype)
-        return { 'lsp', 'treesitter', 'indent' }
+        return { 'treesitter', 'lsp', 'indent' }
       end,
     }
   end,
