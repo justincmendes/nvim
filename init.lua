e163016edd3e2405b1ed3504bbea8d45198229ce
Justin Mendes <justincmendes@gmail.com>
2024-10-28 12:16:22 -0400

    Added ripgrep dependency for telescope (live_grep)

--- init.lua
+++ init.lua
@@ -346,11 +346,12 @@ require('lazy').setup({
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         -- `build` is used to run some command when the plugin is installed/updated.
         -- This is only run then, not every time Neovim starts up.
-        build = 'make',
+
+        -- Ripgrep for live_grep
+        'BurntSushi/ripgrep',
 
         -- `cond` is a condition used to determine whether this plugin should be
         -- installed and loaded.
