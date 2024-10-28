55b1d3632c90ca4f20ed557a3c6711345743f149
Justin Mendes <justincmendes@gmail.com>
2024-10-28 12:19:06 -0400

    Manually install ripgrep or other fuzzy finder on machine instead

--- init.lua
+++ init.lua
@@ -346,12 +346,11 @@ require('lazy').setup({
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        'nvim-telescope/telescope-fzf-native.nvim',
+
         -- `build` is used to run some command when the plugin is installed/updated.
         -- This is only run then, not every time Neovim starts up.
-
-        -- Ripgrep for live_grep
-        'BurntSushi/ripgrep',
+        build = 'make',
 
         -- `cond` is a condition used to determine whether this plugin should be
         -- installed and loaded.
